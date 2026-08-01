82d89ac39559bf065e1191b7a15a26957050feeb
Josee <jhee0141@student.monash.edu>
2026-08-01 17:12:18 +1000

    Initial commit

---
+++ src/main/java/com/josee/aicodemother/AiCodeMotherApplication.java
@@ -0,0 +1,13 @@
+package com.josee.aicodemother;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiCodeMotherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiCodeMotherApplication.class, args);
+	}
+
+}
