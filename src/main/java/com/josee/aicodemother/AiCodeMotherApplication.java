d06b202a1c513e68d8fc8a0c890f4b241908bf74
Josee <jhee0141@student.monash.edu>
2026-08-09 16:32:56 +1000

    后端开发 - 用户模块

--- src/main/java/com/josee/aicodemother/AiCodeMotherApplication.java
+++ src/main/java/com/josee/aicodemother/AiCodeMotherApplication.java
@@ -1,9 +1,11 @@
 package com.josee.aicodemother;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.josee.aicodemother.mapper")
 public class AiCodeMotherApplication {
 
     public static void main(String[] args) {
@@ -11,3 +13,4 @@ public class AiCodeMotherApplication {
     }
 
 }
+
