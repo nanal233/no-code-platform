ba68565cbffc4f0a1a67780e2f24ddd62664890d
Josee <jhee0141@student.monash.edu>
2026-08-15 17:58:10 +1000

    前端开发 - 用户模块

--- ai-code-mother-frontend/src/components/GlobalHeader.vue
+++ ai-code-mother-frontend/src/components/GlobalHeader.vue
@@ -1,21 +1,72 @@
 <script setup lang="ts">
-import { computed } from 'vue'
+import { computed, h } from 'vue'
 import { useRouter, useRoute } from 'vue-router'
-import type { MenuProps } from 'ant-design-vue'
+import { type MenuProps, message } from 'ant-design-vue'
 import logo from '@/assets/logo.png'
+import { useLoginUserStore } from '@/stores/loginUser.ts'
+import { LogoutOutlined } from '@ant-design/icons-vue'
+import { userLogout } from '@/api/userController.ts'
+
+// 获取登录用户状态
+const loginUserStore = useLoginUserStore()
 
 const router = useRouter()
 const route = useRoute()
 
-const menuItems: MenuProps['items'] = [
-  { key: '/', label: '主页' },
-  { key: '/about', label: '关于' },
+// 菜单配置项
+const originItems = [
+  {
+    key: '/',
+    label: '主页',
+    title: '主页',
+  },
+  {
+    key: 'others',
+    label: h('a', {href: 'https://github.com/nanal233', target: '_blank'}, '关于作者'),
+    title: '主页',
+  },
+  {
+    key: '/admin/userManage',
+    label: '用户管理',
+    title: '用户管理',
+  },
 ]
+
+// 过滤菜单项
+const filterMenus = (menus = [] as MenuProps['items']) => {
+  return menus?.filter((menu) => {
+    const menuKey = menu?.key as string
+    if (menuKey?.startsWith('/admin')) {
+      const loginUser = loginUserStore.loginUser
+      if (!loginUser || loginUser.userRole !== 'admin') {
+        return false
+      }
+    }
+    return true
+  })
+}
+
+// 展示在菜单的路由数组
+const menuItems = computed<MenuProps['items']>(() => filterMenus(originItems))
 
 const selectedKeys = computed(() => [route.path])
 
 const handleMenuClick = ({ key }: { key: string }) => {
   router.push(key)
+}
+
+// 用户注销
+const doLogout = async () => {
+  const res = await userLogout()
+  if (res.data.code === 0) {
+    loginUserStore.setLoginUser({
+      userName: '未登录',
+    })
+    message.success('退出登录成功')
+    await router.push('/user/login')
+  } else {
+    message.error('退出登录失败，' + res.data.message)
+  }
 }
 </script>
 
@@ -32,8 +83,26 @@ const handleMenuClick = ({ key }: { key: string }) => {
       class="header-menu"
       @click="handleMenuClick"
     />
-    <div class="header-right">
-      <a-button type="primary">登录</a-button>
+    <div class="user-login-status">
+      <div v-if="loginUserStore.loginUser.id">
+        <a-dropdown>
+          <a-space>
+            <a-avatar :src="loginUserStore.loginUser.userAvatar" />
+            {{ loginUserStore.loginUser.userName ?? '无名' }}
+          </a-space>
+          <template #overlay>
+            <a-menu>
+              <a-menu-item @click="doLogout">
+                <LogoutOutlined />
+                退出登录
+              </a-menu-item>
+            </a-menu>
+          </template>
+        </a-dropdown>
+      </div>
+      <div v-else>
+        <a-button type="primary" href="/user/login">登录</a-button>
+      </div>
     </div>
   </a-layout-header>
 </template>
@@ -77,7 +146,7 @@ const handleMenuClick = ({ key }: { key: string }) => {
   min-width: 0;
 }
 
-.header-right {
+.user-login-status {
   flex-shrink: 0;
   margin-left: 16px;
 }
