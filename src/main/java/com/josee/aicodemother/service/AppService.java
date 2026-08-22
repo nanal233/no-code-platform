f354ddafd6e2c106fa92870cb29bb69737c90729
Josee <jhee0141@student.monash.edu>
2026-08-22 16:12:36 +1000

    应用模块 应用的Nginx部署

--- src/main/java/com/josee/aicodemother/service/AppService.java
+++ src/main/java/com/josee/aicodemother/service/AppService.java
@@ -1,10 +1,12 @@
 package com.josee.aicodemother.service;
 
 import com.josee.aicodemother.model.dto.app.AppQueryRequest;
+import com.josee.aicodemother.model.entity.User;
 import com.josee.aicodemother.model.vo.AppVO;
 import com.mybatisflex.core.query.QueryWrapper;
 import com.mybatisflex.core.service.IService;
 import com.josee.aicodemother.model.entity.App;
+import reactor.core.publisher.Flux;
 
 import java.util.List;
 
@@ -14,6 +16,23 @@ import java.util.List;
  * @author <a href="https://github.com/nanal233">Josee</a>
  */
 public interface AppService extends IService<App> {
+
+    /**
+     * 通过对话生成应用代码
+     * @param appId 应用 ID
+     * @param message 提示词
+     * @param loginUser 登录用户
+     * @return
+     */
+    Flux<String> chatToGenCode(Long appId, String message, User loginUser);
+
+    /**
+     * 应用部署
+     * @param appId 应用 ID
+     * @param loginUser 登录用户
+     * @return 可访问的部署地址
+     */
+    String deployApp(Long appId, User loginUser);
 
     /**
      * 获取应用封装类
@@ -36,8 +55,6 @@ public interface AppService extends IService<App> {
      * @return
      */
     QueryWrapper getQueryWrapper(AppQueryRequest appQueryRequest);
-
-
 
 
 }
