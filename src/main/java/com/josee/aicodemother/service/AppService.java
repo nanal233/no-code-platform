8ba3d23dd23dbcd45be01684c39fface6038d21b
Josee <jhee0141@student.monash.edu>
2026-08-22 14:55:14 +1000

    应用模块（基本功能） 完成基本功能，人工测试通过

--- src/main/java/com/josee/aicodemother/service/AppService.java
+++ src/main/java/com/josee/aicodemother/service/AppService.java
@@ -1,7 +1,12 @@
 package com.josee.aicodemother.service;
 
+import com.josee.aicodemother.model.dto.app.AppQueryRequest;
+import com.josee.aicodemother.model.vo.AppVO;
+import com.mybatisflex.core.query.QueryWrapper;
 import com.mybatisflex.core.service.IService;
 import com.josee.aicodemother.model.entity.App;
+
+import java.util.List;
 
 /**
  * 应用 服务层。
@@ -9,5 +14,30 @@ import com.josee.aicodemother.model.entity.App;
  * @author <a href="https://github.com/nanal233">Josee</a>
  */
 public interface AppService extends IService<App> {
+
+    /**
+     * 获取应用封装类
+     * @param app
+     * @return
+     */
+    AppVO getAppVO(App app);
+
+
+    /**
+     * 获取应用封装列表
+     * @param appList
+     * @return
+     */
+    List<AppVO> getAppVOList(List<App> appList);
+
+    /**
+     * 构造应用查询条件
+     * @param appQueryRequest
+     * @return
+     */
+    QueryWrapper getQueryWrapper(AppQueryRequest appQueryRequest);
+
+
+
 
 }
