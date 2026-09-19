ef8222924179022cede24691d33702dcf4551b5e
Josee <jhee0141@student.monash.edu>
2026-09-19 20:17:55 +1000

    系统优化（稳定性优化）：自动重试护轨，防止工具调用无限循环

--- src/main/java/com/josee/aicodemother/ai/AiCodeGeneratorServiceFactory.java
+++ src/main/java/com/josee/aicodemother/ai/AiCodeGeneratorServiceFactory.java
@@ -108,7 +108,9 @@ public class AiCodeGeneratorServiceFactory {
                         .hallucinatedToolNameStrategy(toolExecutionRequest -> ToolExecutionResultMessage.from(
                                 toolExecutionRequest, "Error: there is no tool called " + toolExecutionRequest.name()
                         ))
+                        .maxSequentialToolsInvocations(20) // 最多连续调用20次工具
                         .inputGuardrails(new PromptSafetyInputGuardrail()) // 添加输入护轨
+                        //.outputGuardrails(new RetryOutputGuardrail()) // 添加输出护轨，为了流式输出，这里不使用
                         .build();
             }
             case HTML, MULTI_FILE -> {
@@ -119,6 +121,7 @@ public class AiCodeGeneratorServiceFactory {
                         .streamingChatModel(openAiStreamingChatModel)
                         .chatMemory(chatMemory)
                         .inputGuardrails(new PromptSafetyInputGuardrail()) // 添加输入护轨
+                        //.outputGuardrails(new RetryOutputGuardrail()) // 添加输出护轨，为了流式输出，这里不使用
                         .build();
             }
             default -> throw new BusinessException(ErrorCode.SYSTEM_ERROR,
